5d7a33bdcc3870aa6c4e0fd2540d9aa3bcdc8cfa
S Venkatesh <216847616+venkatesh10s@users.noreply.github.com>
2026-08-07 01:32:28 +0530

    Initialize counters and variables

--- main.py
+++ main.py
@@ -52,3 +52,31 @@ if profile_input == "yes":
     profile_verified = True
 else:
     profile_verified = False
+
+# ------------------------------------------------------------
+# 2. INITIALIZE COUNTERS AND VARIABLES
+# ------------------------------------------------------------
+
+total_score = 0
+
+attempted_days = 0
+absent_days = 0
+passed_days = 0
+failed_days = 0
+
+strong_days = 0
+satisfactory_days = 0
+improvement_days = 0
+critical_days = 0
+
+highest_score = 0
+highest_score_day = 0
+
+lowest_score = 0
+lowest_score_day = 0
+
+first_attempt_found = False
+
+critical_score_found = False
+first_critical_day = 0
+first_critical_score = 0
